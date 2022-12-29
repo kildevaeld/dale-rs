fecd22559cae1f066680df4f1826627246e67c1e
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2022-12-29 21:06:01 +0100

    example

--- dale/examples/derive.rs
+++ dale/examples/derive.rs
@@ -1,8 +1,19 @@
-use dale::{IntoOutcome, Service};
+use dale::{BoxService, IntoOutcome, IntoService, Service, ServiceExt};
 
 #[derive(IntoOutcome)]
 pub struct Test<A> {
     value: A,
+}
+
+pub struct Serv;
+
+impl<R> IntoService<Test<R>> for Serv {
+    type Error = core::convert::Infallible;
+    type Service = BoxService<'static, Test<R>, i32, core::convert::Infallible>;
+
+    fn into_service(self) -> Result<Self::Service, Self::Error> {
+        Ok((|_| async move { 42 }).boxed())
+    }
 }
 
 fn main() {
