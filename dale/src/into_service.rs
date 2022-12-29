859bbece0fe1b1d0483223f2fdc3577dda3fd529
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2022-12-29 21:05:55 +0100

    imple IntoService for every type impl Service

--- dale/src/into_service.rs
+++ dale/src/into_service.rs
@@ -5,3 +5,15 @@ pub trait IntoService<R> {
     type Service: Service<R>;
     fn into_service(self) -> Result<Self::Service, Self::Error>;
 }
+
+impl<S, R> IntoService<R> for S
+where
+    S: Service<R>,
+{
+    type Error = core::convert::Infallible;
+    type Service = S;
+
+    fn into_service(self) -> Result<Self::Service, Self::Error> {
+        Ok(self)
+    }
+}
